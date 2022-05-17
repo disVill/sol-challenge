fb129a88ebce6d5ac9e27536797f92b4ab098d21
disVill <konokou2000@gmail.com>
2022-05-17 22:46:45 +0900

    Solve GuessTheNumber

--- test/GuessTheNumber.test.ts
+++ test/GuessTheNumber.test.ts
@@ -2,6 +2,7 @@ import { ethers } from 'hardhat'
 import { expect } from 'chai'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { GuessTheNumberChallenge } from '../typechain-types'
+import { BigNumber } from 'ethers'
 
 describe('GuessTheNumberChallenge', async function () {
   let player: SignerWithAddress
@@ -15,8 +16,10 @@ describe('GuessTheNumberChallenge', async function () {
   })
 
   it('Attack', async function () {
-    const a = 0 // input a number
-    const b = 0 // input a number
+    const a = 0
+    const b = BigNumber.from(
+      '115792089237316195423570985008687907853269984665640564039457584007913129638936'
+    )
     await challenge.input(a, b)
     expect(await challenge.isSolved()).to.be.true
   })
